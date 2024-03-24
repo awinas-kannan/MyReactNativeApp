a2d8f3c6cf5d1b13f96bd01e5e925b93c60e755a
Awinas Kannan M R <Awinas.Kannan.Manicka.Rajan@walmart.com>
2024-03-24 10:37:43 +0530

    Init

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicatorRN } from './corecomponents/ActivityIndicatorComponent';
+import { AlertAPIComponent } from './corecomponents/AlertComponent';
+import { ButtonRN } from './corecomponents/ButtonComponent';
+import { GreetArrowCompo, GreetFuncComp } from './corecomponents/Greet';
+import { ImageBGCompRN } from './corecomponents/ImageBackground';
+import { ImageCompRN } from './corecomponents/ImageComponent';
+import { ModalRN } from './corecomponents/ModalComponent';
+import { PressableRN } from './corecomponents/PressableComponent';
+import { ScrollViewRN } from './corecomponents/ScollViewComponent';
+import { StatusBarRN } from './corecomponents/StatusBarComponent';
+import { TextCompRN } from './corecomponents/TextComponent';
+import { ViewCompRN } from './corecomponents/ViewComponent';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+  return (
+
+
+    /*
+     * Core Components And APIS
+    */
+
+
+    // <ViewCompRN/>
+    // <TextCompRN/>
+    // <ImageCompRN/>
+    // <ImageBGCompRN/>
+    // <ScrollViewRN></ScrollViewRN>
+    // <ButtonRN/>
+    // <PressableRN></PressableRN>
+    // <ModalRN></ModalRN>
+    // <StatusBarRN/>
+    // <ActivityIndicatorRN></ActivityIndicatorRN>
+    // <AlertAPIComponent />
+    <>
+      <GreetArrowCompo name="Awinas"></GreetArrowCompo>
+      <GreetFuncComp name="Kannan"></GreetFuncComp>
+    </>
+
+  );
+};
